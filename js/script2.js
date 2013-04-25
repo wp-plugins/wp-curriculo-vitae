feac6dc955c19eb0321fff8f7d5b363ba1dde8b1
Williamluis <Williamluis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-25 12:15:47 +0000

    === WP-Curriculo Vitae === Contributors: Williamluis Tags: curriculo, curriculos, cadastros de curriculo, lista de curriculos,cadastro de usuarios, curriculum, cadastre curriculum, list of
    curriculum, list of resume, cadastre resume
    Requires at least: 3.5
    Tested up to: 3.5.1
    Stable tag: 3.5
    License: GPLv2
    
    == Description ==
    
    O *WP-Curriculo Vitae* possibilita o cadastro de informa&ccedil;&otilde;es profissional do usu&aacute;rio, onde que esse cadastro vai 
    
    se torna publico.
    Ter&aacute; uma lista com as informa&ccedil;&otilde;es dos cadastro, podendo entrar em contato com o cadastrado.
    Na Pr&oacute;pria lista pode buscar um nome ou uma especifica&ccedil;&atilde;o, facilitando a busca.
    
    *Para fazer o formul&aacute;rio de cadastro, crie uma p&aacute;gina e coloque o shortcode **[formCadastro]**
    *Para criar a listagem dos cadastrados, crie uma p&aacute;gina e coloque o shortcode **[listCurriculos]**
    *Use o widget WP-CV para facilitar as busca do cadastrados n&atilde;o precisando abrir a p&aacute;gina de listagem, para ter um bom 
    
    funcionamento dessa widget, precisa colocar o endere&ccedil;o da p&aacute;gina de listagem.
    *Use widget WP-CV para os cadastrados poder fazer login para poderem alterar seu cadastro.
      
      Obs. Os widget s&atilde;o apenas para a vers&atilde;o paga.
    
    == Installation ==
    
    1. Subir o arquivo zipado para o diret&oacute;rio '/wp-content/plugins/'
    2. Ativar o aplicativo
    3. Crie uma p&aacute;ginas colocando o shortcode e publique
    
    
    == Frequently Asked Questions ==
    
    = &Eacute; poss&iacute;vel seugerir modifica&ccedil;&otilde;es e id&eacute;ias para este plugin? =
    
    Claro que pode, para isso acesse o link [Wiliamluis - WP-Curr&iacute;culo Vitae](http://wiliamluis.wordpress.com/plugin/)
    
    == Changelog ==
    
    = 1.0 =
    * Vers&atilde;o teste
    
    == Screenshots ==
    
    
    git-svn-id: https://plugins.svn.wordpress.org/wp-curriculo-vitae/tags/1.0@703445 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/script2.js
@@ -0,0 +1,48 @@
+(function ($) {
+/* Função que carrega script das abas */
+        
+$.abasSimples = function ()
+{
+	/* Guarda IDs dos elementos */
+	var abas = 'p#abas';
+	var conteudos = 'ul#conteudos';
+	
+	/* Oculta todas as abas */
+	$(conteudos + ' li').hide();
+	
+	/* Exibe a primeira aba */
+	<?php if($msg==3){  ?>
+		$(conteudos + ' #aba2').show();
+	<?php }else{ ?>
+		$(conteudos + ' li:first-child').show();
+	<?php } ?>
+
+	/* Quando uma aba for clicada */
+	$(abas + ' a').click(function()
+	{
+		/* Remove todas as classes de todas as abas */
+		$(abas + ' a').removeClass('selected');
+		
+		/* Acrescenta uma classe CSS marcando visualmente a aba como selecionada */
+		$(this).addClass('selected');
+		
+		/* Oculta todas as abas abertas */
+		$(conteudos + ' li').hide();
+		
+		/* Exibe a aba que foi clicada */
+		$(conteudos +  ' ' + $(this).attr('href')).show();
+		
+		/* Fim :D */
+		return false;
+	}); 
+	
+};
+
+/* Quando o documento estiver carregado… */
+$(document).ready(function()
+{
+	/* Carrega a função das abas */
+	$.abasSimples();
+
+});
+}(jQuery));
