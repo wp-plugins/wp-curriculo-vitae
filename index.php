d75430004dc17c7bf8c24ada39bf20c0ba10cbb8
Williamluis <Williamluis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-26 14:23:58 +0000

    === WP-Curriculo Vitae === Contributors: Williamluis Tags: curriculo, curriculos, cadastros de curriculo, lista de curriculos,cadastro de usuarios, curriculum, cadastre curriculum, list of curriculum, list of resume, cadastre resume Requires at least: 3.5 Tested up to: 3.5.1 Stable tag: 3.5 License: GPLv2
    == Description ==
    
    O *WP-Curriculo Vitae* possibilita o cadastro de informa&ccedil;&otilde;es profissional do usu&aacute;rio, onde que esse cadastro vai se torna publico.
    Ter&aacute; uma lista com as informa&ccedil;&otilde;es dos cadastro, podendo entrar em contato com o cadastrado.
    Na Pr&oacute;pria lista pode buscar um nome ou uma especifica&ccedil;&atilde;o, facilitando a busca.
    
    *Para fazer o formul&aacute;rio de cadastro, crie uma p&aacute;gina e coloque o shortcode **[formCadastro]**
    *Para criar a listagem dos cadastrados, crie uma p&aacute;gina e coloque o shortcode **[listCurriculos]**
    *Use o widget WP-CV para facilitar as busca do cadastrados n&atilde;o precisando abrir a p&aacute;gina de listagem, para ter um bom funcionamento dessa widget, precisa colocar o endere&ccedil;o da p&aacute;gina de listagem.
    *Use widget WP-CV para os cadastrados poder fazer login para poderem alterar seu cadastro.
      
      Obs. Os widget s&atilde;o apenas para a vers&atilde;o paga.
    
    == Installation ==
    
    1. Subir o arquivo zipado para o diret&oacute;rio '/wp-content/plugins/'
    2. Ativar o aplicativo
    3. Crie uma p&aacute;ginas colocando o shortcode e publique
    
    
    == Frequently Asked Questions ==
    
    = &Eacute; poss&iacute;vel seugerir modifica&ccedil;&otilde;es e id&eacute;ias para este plugin? =
    
    Claro que pode, para isso acesse o link [Wiliamluis - WP-Curr&iacute;culo Vitae](http://wiliamluis.wordpress.com/plugin/)
    
    == Changelog ==
    
    = 1.0 =
    *Essa vers&atilde;o vem com dois  shortcode:
    ***[formCadastro]** para criar um formul&aacute;rio de cadastro;
    ***[listCurriculos]** para criar uma lista dos cadastrados.
    *Um painel que est&aacute; em ferramentas Configura&ccedil;&otilde;es-&gt;WP-Curr&iacute;culo Vitae, onde tem as informa&ccedil;&otilde;es do plugin e um email para suporte, uma lista com todos os registros cadastrado, com as op&ccedil;&otilde;es para alterar registro e deletar o mesmo.
    
    == Screenshots ==
    
    
    git-svn-id: https://plugins.svn.wordpress.org/wp-curriculo-vitae/trunk@704231 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ Plugin URI: http://wiliamluis.wordpress.com
 Description: O *WP-Curriculo Vitae* possibilita o cadastro de informações profissional do usuário, onde que esse cadastro vai se torna publico.
 Terá uma lista com as informações dos cadastro, podendo entrar em contato com o cadastrado.
 Na Própria lista pode buscar um nome ou uma especificação, facilitando a busca.
-Version: 1.0
 Author: William Luis da Silva
+Version: 1.0
 Author URI: http://wiliamluis.wordpress.com/plugin/
 License: GPLv2
 */
