895d0fe091c62b39f677010ae3dcfbb97773c5eb
Williamluis <Williamluis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-09 11:37:26 +0000

    Atualizações dos arquivos index.php e readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/wp-curriculo-vitae/trunk@710308 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- informativo.php
+++ informativo.php
@@ -4,13 +4,13 @@ wp_enqueue_style( 'bootstrap', plugins_url('css/bootstrap.css', __FILE__));
 wp_enqueue_style( 'responsive', plugins_url('css/bootstrap-responsive.css', __FILE__) );
 wp_enqueue_style( "prettyPhotoCSS", plugins_url('css/prettyPhoto.css', __FILE__) );
 
-wp_enqueue_script( 'jquery');	
+wp_enqueue_script( 'jquery');
 wp_enqueue_script( 'bootstrapJS', plugins_url('js/bootstrap.min.js', __FILE__));
 wp_enqueue_script( 'prettyPhotoJS', plugins_url('js/jquery.prettyPhoto.js', __FILE__));
 
 ?>
 <div class="container-fluid">
-    <h2>Informações do plugin</h2>					
+    <h2>Informações do plugin</h2>
     <p>O <strong>WP-Curriculo Vitae</strong> e um plugin que permite que usuários a cadastrem seu currículo no site para divulgação online ou para uso do site.</p>
     <p>O plugin trabalha com duas versoes:</p>
     <p><strong>Versão gratuita:</strong></p>
@@ -23,16 +23,17 @@ wp_enqueue_script( 'prettyPhotoJS', plugins_url('js/jquery.prettyPhoto.js', __FI
     3. Widget de login na área restrita;<br>
     4. Listagem de currículos com sistema de busca;<br>
     5. Widget de Busca de Currículos.</p>
-    
+
     <p>Para comprar a versão paga clique no botão abaixo:</p>
-    <!-- INICIO FORMULARIO BOTAO PAGSEGURO -->
+
+    <?php /*<!-- INICIO FORMULARIO BOTAO PAGSEGURO -->
 <form target="pagseguro" action="https://pagseguro.uol.com.br/checkout/v2/payment.html" method="post">
 <!-- NÃO EDITE OS COMANDOS DAS LINHAS ABAIXO -->
 <input type="hidden" name="code" value="2A503DF5D6D66EDFF46EBFA2EED8B23A" />
 <input type="image" src="https://p.simg.uol.com.br/out/pagseguro/i/botoes/pagamentos/164x37-comprar-assina.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
 </form>
-<!-- FINAL FORMULARIO BOTAO PAGSEGURO -->
-    
+<!-- FINAL FORMULARIO BOTAO PAGSEGURO -->*/ ?>
+
     <p><strong>Como utilizar</strong></p>
     <p>Para exibir o formulário de cadastro, crie uma pagina e coloque o shortcode <strong>[formCadastro]</strong>.</p>
     <p>Para exibir a listagem dos cadastrados, crie uma página e coloque o shortcode <strong>[listCurriculos]</strong>.</p>
