62795fdf2f820376eac6a572322742ae15818feb
Williamluis <Williamluis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-21 21:32:52 +0000

    atualização do arquivo uninstall.php
    git-svn-id: https://plugins.svn.wordpress.org/wp-curriculo-vitae/tags/1.4@716241 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -2,8 +2,8 @@
  
 // Vamos garantir que é o WordPress que chama este ficheiro
 // e que realmente está a desistalar o plugin.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
-  die();
+#if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+ # die();
  
 
 // Acesso ao objeto global de gestão de bases de dados
@@ -12,27 +12,21 @@ global $wpdb;
 // Vamos checar se a nova tabela existe
 // A propriedade prefix é o prefixo de tabela escolhido na
 // instalação do WordPress
-$tablename = $wpdb->prefix . 'wls_curriculo';
+
 
 // Se a tabela não existe vamos criá-la
-if ( $wpdb->get_var( "SHOW TABLES LIKE '$tablename'" ) != $tablename ) {
 
-  $sql = "DROP TABLE wls_curriculo;";
-
-  // Para usarmos a função dbDelta() é necessário carregar este ficheiro
-  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-
-  dbDelta( $sql );
+  $sql = "DROP TABLE wls_curriculo";
+  $wpdb->query($sql);
   
+  $sql = "DROP TABLE wls_areas";
+  $wpdb->query($sql);
+
   $upload = wp_upload_dir();
   $upload_dir = $upload['basedir'];
   $upload_dir = $upload_dir . '/curriculos';
   
   @rmdir($upload_dir);
-}
 
- 
-// Vamos remover as opções que criámos na instalação
-#delete_option( 'ewp_opcao' );
  
 ?>
