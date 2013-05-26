cbb9fc1c27eea484d578951a6f54cb66dc95b10a
Williamluis <Williamluis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-26 03:06:15 +0000

    Atualização para subir o arquivo
    git-svn-id: https://plugins.svn.wordpress.org/wp-curriculo-vitae/tags/1.0@718380 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- install.php
+++ install.php
@@ -6,21 +6,29 @@ global $wpdb;
 // A propriedade prefix é o prefixo de tabela escolhido na
 // instalação do WordPress
 $tablename = $wpdb->prefix . 'wls_curriculo';
+$tablename = $wpdb->prefix . 'wls_area';
 
 // Se a tabela não existe vamos criá-la
-if ( $wpdb->get_var( "SHOW TABLES LIKE '$tablename'" ) != $tablename ) {
+if ( $wpdb->get_var( "SHOW TABLES LIKE '$tablename'" ) != $tablename || $wpdb->get_var( "SHOW TABLES LIKE '$tablename'" ) != $tablename ) {
 
   $sql = "CREATE TABLE wls_curriculo (
 			id int(11) NOT NULL AUTO_INCREMENT,
+			id_area int(255),
 			nome varchar(255) COLLATE latin1_bin DEFAULT NULL,
 			descricao text COLLATE latin1_bin,
 			login varchar(255) COLLATE latin1_bin DEFAULT NULL,
 			senha varchar(255) COLLATE latin1_bin DEFAULT NULL,
 			cpf varchar(255) COLLATE latin1_bin DEFAULT NULL,
 			email varchar(255) COLLATE latin1_bin DEFAULT NULL,
-			file varchar(255) COLLATE latin1_bin DEFAULT NULL,
+			curriculo varchar(255) COLLATE latin1_bin DEFAULT NULL,
 			PRIMARY KEY (`id`)
-		  );";
+		  );
+		  
+		CREATE TABLE wls_areas (
+			id int(11) NOT NULL AUTO_INCREMENT,
+			area varchar(255) COLLATE latin1_bin DEFAULT NULL,
+			PRIMARY KEY (`id`)
+	  	)";
 
   // Para usarmos a função dbDelta() é necessário carregar este ficheiro
   require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
@@ -34,7 +42,7 @@ if ( $wpdb->get_var( "SHOW TABLES LIKE '$tablename'" ) != $tablename ) {
   $upload_dir = $upload_dir . '/curriculos';
   
   if (! is_dir($upload_dir)) {
-	 mkdir( $upload_dir, 0777 );
+	 mkdir( $upload_dir, 777 );
   }
 
 }
