1042c0600655db6632930392b53341ec05aabe6b
Williamluis <Williamluis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-18 18:43:23 +0000

    1. Ajuste no menu configuracoes de e-mails;
    git-svn-id: https://plugins.svn.wordpress.org/wp-curriculo-vitae/trunk@951181 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-Currículo Vitae Free
 Plugin URI: http://wiliamluis.wordpress.com/plugin/
 Description: O WP-Curriculo Vitae e um plugin que permite que usuarios a cadastrem seu curriculo no site para divulgacao online ou para uso do site.
-Version: 4.5.4
+Version: 4.5.5
 Author: William Luis da Silva
 Author URI: http://www.williamluis.com.br/wp-cv-demonstracao/
 License: GPLv2
